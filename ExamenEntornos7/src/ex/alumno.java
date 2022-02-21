4125ff43dc5a191c308e94b4e48e016aa8e1251d
Alumno21-22-04 <Alumno21-22-04@DESKTOP-J2P4HGQ>
2022-02-21 13:12:05 +0100

    cambio1

---
+++ ExamenEntornos7/src/ex/alumno.java
@@ -0,0 +1,19 @@
+package ex;
+
+public class alumno {
+String nombre;
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public int getEdad() {
+	return edad;
+}
+public void setEdad(int edad) {
+	this.edad = edad;
+}
+int edad;
+}
+
